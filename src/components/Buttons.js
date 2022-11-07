775da307f8a01f8ef7bddcdd279424871f3409a4
unknown <judeashitey4@gmail.com>
2022-11-07 15:59:18 -0500

    Checking the props

---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+const Button = ()=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Button;
---
+++ src/components/Questionare.js
@@ -0,0 +1,24 @@
+const Query = (props)=>{
+     const {question} = props;
+  
+  
+  
+  
+  // ================= Display Section =================
+    return(
+    <div>
+
+        <h1>Category :</h1>
+        <h2>Points :</h2>
+        <h2>Answer :</h2>
+
+    </div> 
+    )
+}
+
+
+
+
+
+
+export default Query;
